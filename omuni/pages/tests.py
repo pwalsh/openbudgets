8b8ec01f9240a037d3259d348bb64109803336e0
Paul Walsh <paulywalsh@gmail.com>
2013-02-03 14:47:26 +0200

    Basic tests for pages.Page in Engloish and Hebrew, testing for slugs, templates and 404s.

--- omuni/pages/tests.py
+++ omuni/pages/tests.py
@@ -1,1 +1,28 @@
+# -*- coding: utf-8 -*-
+
 from django.test import TestCase
+
+
+class PageTestCase(TestCase):
+    """Tests for pages.Page objects and whatever touches them"""
+
+    fixtures = ['test_pages.json']
+
+    def test_en_page(self):
+        """Test pages with English slugs, and check template."""
+        response = self.client.get('/test/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('page' in response.context)
+        self.assertContains(response, 'A test page for English pages.')
+
+    def test_he_page(self):
+        """Test pages with Hebrew slugs, and check template."""
+        response = self.client.get('/בדיקה/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('page' in response.context)
+        self.assertContains(response, 'בדיקה לדפיים עבריים.')
+
+    def test_404_page(self):
+        """Does the 404 page work?"""
+        response = self.client.get('/this-page-can-not-possibly-exist-here/')
+        self.assertEqual(response.status_code, 404)
