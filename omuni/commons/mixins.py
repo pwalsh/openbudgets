5df57018054bf41d9a90b2af0e77ee4a94b9a1cc
Paul Walsh <paulywalsh@gmail.com>
2013-02-04 11:14:06 +0200

    Simple mixin for CBVs, where the object user must match the request user.

---
+++ omuni/commons/mixins.py
@@ -0,0 +1,16 @@
+from django.core.exceptions import PermissionDenied
+from django.views.generic.detail import SingleObjectMixin
+
+
+class UserDataObjectMixin(SingleObjectMixin):
+    """Returns a data object only if it belongs to the request user.
+
+    This mixin is only for use with objects that have a foreign key to User.
+    """
+    def get_object(self, queryset=None, castable=True):
+        obj = super(UserDataObjectMixin, self).get_object(queryset)
+
+        if obj.user_id != self.request.user.id:
+            raise PermissionDenied
+
+        return obj
