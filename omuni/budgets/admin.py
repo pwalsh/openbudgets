4b6a250e17db8c54bea398f02d833f27d358548c
Paul Walsh <paulywalsh@gmail.com>
2013-02-12 21:20:26 +0200

    Added inlined forms for datasources on BUdget Templates and Budgets

--- omuni/budgets/admin.py
+++ omuni/budgets/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from omuni.budgets.models import BudgetTemplate, BudgetTemplateNode, Budget, BudgetItem
+from omuni.commons.admin import DataSourceInline
 
 
 class BudgetTemplateNodeInline(admin.StackedInline):
@@ -8,14 +9,14 @@ class BudgetTemplateNodeInline(admin.StackedInline):
     model = BudgetTemplateNode
     fk_name = 'template'
     classes = ('grp-collapse grp-open',)
-    inline_classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-closed',)
     extra = 2
 
 
 class BudgetTemplateAdmin(admin.ModelAdmin):
     """Admin form for adding budget templates"""
 
-    inlines = [BudgetTemplateNodeInline]
+    inlines = [BudgetTemplateNodeInline, DataSourceInline]
 
 
 class BudgetItemInline(admin.StackedInline):
@@ -24,14 +25,14 @@ class BudgetItemInline(admin.StackedInline):
     model = BudgetItem
     fk_name = 'budget'
     classes = ('grp-collapse grp-open',)
-    inline_classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-closed',)
     extra = 2
 
 
 class BudgetAdmin(admin.ModelAdmin):
     """Admin form for adding budgets"""
 
-    inlines = [BudgetItemInline]
+    inlines = [BudgetItemInline, DataSourceInline]
 
 
 admin.site.register(BudgetTemplate, BudgetTemplateAdmin)
--- omuni/commons/admin.py
+++ omuni/commons/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from django.contrib.contenttypes import generic
 #from djcelery.models import TaskState, WorkerState, PeriodicTask, IntervalSchedule, CrontabSchedule
+from omuni.commons.models import DataSource
+
+
+class DataSourceInline(generic.GenericStackedInline):
+    """Gives an inlineable DataSource form"""
+
+    model = DataSource
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
+    extra = 0
 
 
 # Celery admin config
