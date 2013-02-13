fb9d2a77a20f1e131121770e6afef2c11cfe5e88
Yehonatan Daniv <maggotfish@gmail.com>
2013-02-13 11:14:55 +0200

    Fixed import of UUIDModel

--- omuni/accounts/models.py
+++ omuni/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from omuni.commons.mixins import UUIDModel
+from omuni.commons.mixins.models import UUIDModel
 from omuni.settings import LANGUAGES
 
 
