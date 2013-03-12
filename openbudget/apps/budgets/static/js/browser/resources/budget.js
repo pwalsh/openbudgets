e95cf413398e6d7e3f89146743bb0e9900bfb634
Yehonatan Daniv <maggotfish@gmail.com>
2013-03-13 00:15:57 +0200

    Continue implementation of Budget Browser

--- openbudget/apps/budgets/static/js/browser/resources/budget.js
+++ openbudget/apps/budgets/static/js/browser/resources/budget.js
@@ -1,9 +1,25 @@
 define([
-    'uijet_dir/uijet'
+    'uijet_dir/uijet',
+    'browser/app'
 ], function (uijet) {
 
-    uijet.Resource('Budget', uijet.Model({
-        
-    }));
+    var BASE_API_URL = 'http://api.obudget.dev:8000/',
+        BudgetModel = uijet.Model({
+            idAttribute : 'uuid'
+        }),
+        BudgetsCollection = uijet.Collection({
+            model   : BudgetModel,
+            url     : BASE_API_URL + 'budgets/',
+            parse   : function (response) {
+                return response.results;
+            }
+        }),
+        exports = {
+            budget_model        : BudgetModel,
+            budgets_collection  : BudgetsCollection
+        };
 
+    uijet.Resource('Budgets', BudgetsCollection);
+
+    return exports;
 });
--- openbudget/apps/budgets/static/js/browser/ui/main.js
+++ openbudget/apps/budgets/static/js/browser/ui/main.js
@@ -4,16 +4,54 @@ define([
 ], function (uijet) {
 
     uijet.declare([{
+        type    : 'Pane',
+        config  : {
+            element         : '#heading',
+            position        : 'top:50',
+            wake_on_startup : true
+        }
+    }, {
+        type    : 'List',
+        config  : {
+            element         : '#budgets_list',
+            mixins          : ['Templated'],
+            resource        : 'Budgets',
+            wake_on_startup : true,
+            position        : 'top:50|50',
+            signals         : {
+                pre_select  : function ($selected) {
+                    return this.resource.get($selected[0].id).toJSON();
+                }
+            }
+        }
+    }, {
         type    : 'Table',
         config  : {
-            element : '#spreadsheet',
-            head    : {
+            element     : '#spreadsheet',
+            head        : {
                 columns : ['code', 'name', 'amount', 'description']
             },
-            body    : {
+            grid        : {
                 mixins          : ['Templated'],
-                resource        : 'Budget',
-                template_name   : 'spreadsheet-body'
+                template_name   : 'spreadsheet-grid',
+                signals         : {
+                    pre_wake    : function () {
+                        return false;
+                    }
+                },
+                app_events      : {
+                    'budgets_list.selected' : function (data) {
+                        var that = this;console.log(data);
+                        this.data = data;
+                        this.render().then(function () {
+                            that.publish('rendered');
+                        });
+                    }
+                }
+            },
+            position    : 'fluid',
+            app_events  : {
+                'spreadsheet_grid.rendered' : 'wake'
             }
         }
     }]);
