ddd48a762c32c8cf8a51cc90571b3f91b5c9189a
Yehonatan Daniv <maggotfish@gmail.com>
2013-03-20 11:15:59 +0200

    Improvements to spreadsheet and initial implementation of graph

--- openbudget/apps/budgets/static/js/browser/ui/main.js
+++ openbudget/apps/budgets/static/js/browser/ui/main.js
@@ -1,24 +1,24 @@
 define([
     'uijet_dir/uijet',
-    'resources/budget'
+    'resources/budget',
+    'widgets/Graph',
+    'adapters/RickshawGraph'
 ], function (uijet) {
 
     uijet.declare([{
         type    : 'Pane',
         config  : {
-            element         : '#heading',
-            position        : 'top:50',
-            wake_on_startup : true
+            element : '#heading',
+            position: 'top:50'
         }
     }, {
         type    : 'List',
         config  : {
-            element         : '#budgets_list',
-            mixins          : ['Templated'],
-            resource        : 'Budgets',
-            wake_on_startup : true,
-            position        : 'top:50|50',
-            signals         : {
+            element : '#budgets_list',
+            mixins  : ['Templated'],
+            resource: 'Budgets',
+            position: 'top:70|50',
+            signals : {
                 pre_select  : function ($selected) {
                     return this.resource.get($selected[0].id).toJSON();
                 }
@@ -28,6 +28,8 @@ define([
         type    : 'Table',
         config  : {
             element     : '#spreadsheet',
+            dont_wake   : true,
+            mixins      : ['Layered'],
             head        : {
                 columns : ['code', 'name', 'amount', 'description']
             },
@@ -35,23 +37,50 @@ define([
                 mixins          : ['Templated'],
                 template_name   : 'spreadsheet-grid',
                 signals         : {
-                    pre_wake    : function () {
-                        return false;
+                    pre_render      : function () {
+                        this.$element[0].style.opacity = 0;
+                    },
+                    post_rowsinit   : function () {
+                        this.$element[0].style.opacity = 1;
                     }
                 },
                 app_events      : {
                     'budgets_list.selected' : function (data) {
-                        var that = this;
                         this.data = data;
-                        this.render().then(function () {
-                            that.publish('rendered');
-                        });
+                        this.publish('wake', true);
                     }
                 }
             },
             position    : 'fluid',
             app_events  : {
-                'spreadsheet_grid.rendered' : 'wake'
+                'spreadsheet_grid.wake' : 'wake+'
+            }
+        }
+    }, {
+        type    : 'Graph',
+        config  : {
+            element     : '#graph',
+            adapters    : 'RickshawGraph',
+            mixins      : ['Layered'],
+            dont_wake   : true,
+            graph       : {
+                renderer: 'stack',
+                scheme  : 'classic9',
+                axis    : 'time'
+            },
+            position    : 'fluid',
+            resource    : 'Budgets',
+            app_events  : {
+                'spreadsheet.selected'  : function (selected) {
+                    var data = this.resource.chartData(selected.row.index());
+                    this.options.graph.series = [{
+                        data    : data,
+                        color   : this.palette.color()
+                    }];
+                    this.changed = true;
+
+                    this.wake(true);
+                }
             }
         }
     }]);
