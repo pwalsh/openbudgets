1987e7cfe6e89d83dbfe0df321bdca85978502b8
Yehonatan Daniv <maggotfish@gmail.com>
2013-03-21 17:47:41 +0200

    Implemented selftest to quickly run tests only of the apps configured in settings.DEVSTRAP.TESTS

---
+++ openbudget/commons/management/commands/selftest.py
@@ -0,0 +1,15 @@
+import os
+from optparse import make_option
+from django.core.management import call_command
+from django.core.management.base import BaseCommand, CommandError
+from openbudget.settings import local as settings
+
+
+class Command(BaseCommand):
+    help = 'Test only the apps configured in settings.DEVSTRAP.TESTS'
+    
+    def handle(self, *args, **options):
+        self.stdout.write("### DON'T PANIC\n")
+        self.stdout.write("### Testing just what you asked for\n")
+
+        call_command('test', *settings.DEVSTRAP['TESTS'])
