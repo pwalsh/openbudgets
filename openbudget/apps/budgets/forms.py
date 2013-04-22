256a7e2a5570aaf30dfc763049e2e29459f2d3ba
Paul Walsh <paulywalsh@gmail.com>
2013-04-22 15:15:35 +0300

    Removed, was not needed

--- openbudget/apps/budgets/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.utils.translation import ugettext_lazy as _
-from openbudget.apps.budgets.models import Annotation
-
-
-class AnnotationForm(forms.ModelForm):
-
-    class Meta:
-        model = Annotation
