0e28e3d415ee141bcdac182ed044dca87be8e940
Paul Walsh <paulywalsh@gmail.com>
2013-04-22 10:07:18 +0300

    Removed TODO and added an issue in tracker for the same

--- openbudget/apps/entities/views.py
+++ openbudget/apps/entities/views.py
@@ -14,23 +14,3 @@ class EntityListView(ListView):
 class EntityDetailView(DetailView):
     model = Entity
     template_name = 'entities/entity_detail.html'
-
-    # TODO: get object when slug is more complex
-    # We want things like:
-    # /state/country/budget/year
-    # /state/country
-    # /state/country/budget/year
-    # /state/country/actual/year
-    # couldn't get it working in a clean way with a nice API.
-
-    #def get_object(self, queryset=None):
-    #    print 'get object'
-    #    real_slug = self.kwargs['slug'].split(',')[-1]
-    #    print real_slug
-    #    queryset = super(EntityDetailView, self).get_queryset()
-    #    print queryset
-    #    queryset = queryset.filter(slug=real_slug)
-    #    print 'this'
-    #    print queryset
-    #    obj = super(EntityDetailView, self).get_object(queryset)
-    #    return obj
