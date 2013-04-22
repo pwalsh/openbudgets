0b653eb8c423d323fed8958a901e206ccaba4b1a
Paul Walsh <paulywalsh@gmail.com>
2013-04-22 11:30:16 +0300

    Created DataExporter class stub

--- openbudget/apps/transport/outgoing.py
+++ openbudget/apps/transport/outgoing.py
@@ -1,20 +1,7 @@
-"""functions for handling files of data coming out the system."""
-
-
+import os
+import datetime
 import tablib
 
 
-def dataset_from_db():
-    pass
-
-
-def databook_from_datasets(datasets):
-    pass
-
-
-def dataset_to_csv(dataset):
-    pass
-
-
-def databook_to_excel(databook):
+class DataExporter(object):
     pass
