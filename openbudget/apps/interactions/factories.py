560e2152e578b21cca64a2cd98558b446d34a9a0
Paul Walsh <paulywalsh@gmail.com>
2013-04-25 12:58:14 +0300

    Actually got factories for my last commit

--- openbudget/apps/interactions/factories.py
+++ openbudget/apps/interactions/factories.py
@@ -1,1 +1,31 @@
-factories.py
+import datetime
+import factory
+from django.utils.timezone import utc
+#from django.contrib.contenttypes.models import ContentType
+from openbudget.apps.accounts.factories import AccountFactory
+from openbudget.apps.interactions.models import Star, Follow, Share
+
+
+class InteractionFactory(factory.Factory):
+
+    user = factory.SubFactory(AccountFactory)
+    content_type = 1
+    object_id = 1
+    last_login = factory.Sequence(
+        lambda n: datetime.datetime.utcnow().replace(tzinfo=utc)
+    )
+    date_joined = factory.Sequence(
+        lambda n: datetime.datetime.utcnow().replace(tzinfo=utc)
+    )
+
+
+class StarFactory(InteractionFactory):
+    FACTORY_FOR = Star
+
+
+class FollowFactory(InteractionFactory):
+    FACTORY_FOR = Follow
+
+
+class ShareFactory(InteractionFactory):
+    FACTORY_FOR = Share
