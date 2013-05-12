0996a15e6c2e61fa514b8939eb25aa6da6f7f88e
Paul Walsh <paulywalsh@gmail.com>
2013-05-12 15:51:54 +0300

    Context factory, and a close to pointless test.

---
+++ openbudget/apps/contexts/factories.py
@@ -0,0 +1,19 @@
+import datetime
+import factory
+from django.utils.timezone import utc
+from openbudget.apps.entities.factories import EntityFactory
+from openbudget.apps.contexts.models import Context
+
+
+class ContextFactory(factory.DjangoModelFactory):
+
+    FACTORY_FOR = Context
+
+    entity = factory.SubFactory(EntityFactory)
+    data = '{"population":0,"ground_surface":0;"high_schools":0}'
+    period_start = factory.Sequence(
+        lambda n: datetime.datetime.utcnow().replace(tzinfo=utc)
+    )
+    period_end = factory.Sequence(
+        lambda n: datetime.datetime.utcnow().replace(tzinfo=utc)
+    )
