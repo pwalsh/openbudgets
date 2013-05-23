089888d17c111692bca58e02422af1281b6fd4a8
Yehonatan Daniv <maggotfish@gmail.com>
2013-05-23 10:56:14 +0300

    Refactored tablib importer into a new file; Fixed filename parsing in BaseImporter._get_parser_from_filename; Fixed docs in BaseImporter + some cleanup

--- openbudget/apps/transport/incoming/importers/__init__.py
+++ openbudget/apps/transport/incoming/importers/__init__.py
@@ -1,8 +1,4 @@
 import os
-from datetime import datetime
-import tablib
-from django.core.mail import send_mail
-from openbudget.settings.base import TEMP_FILES_DIR, ADMINS, EMAIL_HOST_USER
 from openbudget.apps.transport.models import String
 from openbudget.apps.transport.incoming.parsers import get_parser, get_parser_key
 
@@ -39,7 +35,7 @@ class BaseImporter(object):
     def extract(self):
         """Create a valid dataset from data in the sourcefile.
 
-        We create a tablib.Dataset object.
+        We create a dataset object.
 
         We clean up the headers.
 
@@ -98,7 +94,7 @@ class BaseImporter(object):
         "metadata" can be filled out in a form by the person doing \
         the import.
 
-        Alternatively, the DataImporter class also supports parsing \
+        Alternatively, the BaseImporter class also supports parsing \
         the filename of the sourcefile to extract the required \
         metadata. For this option, dataset_meta_in_filename must be \
         True. Importing like this is handy during development, \
@@ -141,7 +137,7 @@ class BaseImporter(object):
         are comma separated.
 
         EXAMPLE - BUDGET TEMPLATE:
-        budgettemplate_name=israel-municipality;divisions=4,5,6.csv
+        budgettemplate_name=israel-municipality;divisions=4,5,6;period_start=2001-01-10.csv
 
         EXAMPLE - BUDGET OR ACTUAL:
         budget_entity=1,period_start=2001-01-01;period_end=2001-12-31.csv
@@ -155,28 +151,29 @@ class BaseImporter(object):
         keys, ext = os.path.splitext(unicode(self.sourcefile))
 
         # first, split the parser key from the container_object keys
-        parser_key, tmp = keys.split('_')
+        parser_key, attributes_str = keys.split('_', 1)
 
+        # get the appropriate parser class
         parser = get_parser(parser_key)
 
         # now get the keyword arguments for the container object
-        container_object_kwargs = tmp.split(';')
+        container_object_kwargs = attributes_str.split(';')
 
-        # and split this keyword arguments into attributes and values
+        # and split these keyword arguments into keys and values
         for kwarg in container_object_kwargs:
-            attr, val = kwarg.split('=')
+            attr_key, attr_val = kwarg.split('=')
 
             # and make sure each attribute is valid for the container model
             try:
-                getattr(parser.container_model, attr)
+                getattr(parser.container_model, attr_key)
             except AttributeError as e:
                 raise e
 
             # if the value has commas, it is an m2m related field
-            if ',' in val:
-                val = tuple(val.split(','))
+            if ',' in attr_val:
+                attr_val = tuple(attr_val.split(','))
 
-            container_object_dict[attr] = val
+            container_object_dict[attr_key] = attr_val
 
         # return instantiated parser
         return parser(container_object_dict)
@@ -191,104 +188,38 @@ class BaseImporter(object):
         #TODO: refactor the meta extraction from string parsing to proper data in POST
         container_object_dict = {}
         parser_key = self.post_data.get('type', 'budget')
-        attributes = self.post_data.get('attributes', None)
+        attributes_str = self.post_data.get('attributes', None)
 
+        # get the appropriate parser class
         parser = get_parser(parser_key)
 
-        if attributes:
-            attributes = attributes.split(';')
+        if attributes_str:
+            # parse the attributes
+            # split the string to pairs
+            attributes = attributes_str.split(';')
             for attr in attributes:
-                key, val = attr.split('=')
+                # split each pair to key-value
+                attr_key, attr_val = attr.split('=')
 
+                # check that this key belongs to our parser's container model
                 try:
-                    getattr(parser.container_model, key)
+                    getattr(parser.container_model, attr_key)
                 except AttributeError as e:
                     try:
-                        getattr(parser.container_model(), key)
+                        # if it's not an attribute of the container model's class
+                        # it might be an attribute of its instance (because of use of mixins)
+                        getattr(parser.container_model(), attr_key)
                     except AttributeError as e:
                         raise e
 
                 # if the value has commas, it is an m2m related field
-                if ',' in val:
-                    val = tuple(val.split(','))
+                if ',' in attr_val:
+                    attr_val = tuple(attr_val.split(','))
 
-                container_object_dict[key] = val
+                container_object_dict[attr_key] = attr_val
 
-        # return instantiated parser
-        return parser(container_object_dict)
+            # return instantiated parser
+            return parser(container_object_dict)
 
-
-class TablibImporter(BaseImporter):
-
-    def get_data(self, stream):
-        try:
-            raw_dataset = tablib.import_set(stream)
-        except AttributeError as e:
-            # TODO: need to get more specific exception
-            self._import_error()
-            raise e
-
-        # `_normalize_headers` may transform the headers and returns the same data object
-        # we then return the dict view of the data
-        return self._normalize_headers(raw_dataset).dict
-
-    def _normalize_headers(self, dataset):
-        """Clean the headers of the dataset.
-
-        We replace the existing headers with new ones that \
-        have been cleaned and normalized.
-
-        To clean, we strip white space and common joining \
-        symbols, and we convert all to lowercase.
-
-        To normalize, we match the header to strings in a \
-        string scope map, and convert to the string in the \
-        map when our key is either the string or in the scope \
-        list.
-
-        """
-        #TODO: see if this method can be moved to the BaseImporter in a more generic way
-        symbols = {
-            ord('_'): None,
-            ord('-'): None,
-            ord('"'): None,
-            ord(' '): None,
-            ord("'"): None,
-        }
-        scopes_map = self._get_header_scopes()
-
-        for index, header in enumerate(dataset.headers):
-
-            tmp = unicode(header).translate(symbols).lower()
-
-            for k, v in scopes_map.iteritems():
-
-                if tmp == k or tmp in v:
-                    dataset.headers[index] = k
-
-        return dataset
-
-    def _import_error(self):
-        """
-        Handle import exception raised by calling `tablib.import_set`
-        """
-
-        dt = datetime.now().isoformat()
-
-        this_file = TEMP_FILES_DIR + \
-            '/failed_import_{timestamp}_{filename}'.format(
-                timestamp=dt,
-                filename=unicode(self.sourcefile)
-            )
-
-        with open(this_file, 'wb+') as tmp_file:
-            for chunk in self.sourcefile.chunks():
-                tmp_file.write(chunk)
-
-        # email ourselves that we have a file to check
-        subject = 'Open Budget: Failed File Import'
-        message = 'The file is attached.'
-        sender = EMAIL_HOST_USER
-        recipients = [ADMINS]
-
-        send_mail(subject, message, sender, recipients)
+        else:
+            raise Exception('No attributes given in meta data.')
