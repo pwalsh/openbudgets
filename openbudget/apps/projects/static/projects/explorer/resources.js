a951b2cef3172ca6905b4e1d5914be88b0363938
Yehonatan Daniv <maggotfish@gmail.com>
2013-05-31 14:39:36 +0300

    Continue implementation of Budget Explorer Project

--- openbudget/apps/projects/static/projects/explorer/resources.js
+++ openbudget/apps/projects/static/projects/explorer/resources.js
@@ -20,8 +20,50 @@ define([
             parse   : function (response) {
                 //! Array.prototype.filter
                 return response.results.filter(function (item) {
-                    return item.division.index === 3;
+                    return item.division.index === 3 && (item.budgets.length || item.actuals.length);
                 });
+            }
+        }),
+        /*
+         * Budget Model
+         */
+        Budget = uijet.Model({
+            idAttribute : 'uuid'
+        }),
+        /*
+         * Budgets Collection
+         */
+        Budgets = uijet.Collection({
+            model   : Budget,
+            parse   : function (response) {
+                var budgets = response.budgets;
+                uijet.publish('budgets_updated', {
+                    collection  : this,
+                    budgets     : budgets
+                });
+                return budgets;
+            },
+            setUrl  : function (entity_url) {
+                this.url = entity_url;
+            }
+        }),
+
+        /*
+         * Actual Model
+         */
+        Actual = uijet.Model({
+            idAttribute : 'uuid'
+        }),
+        /*
+         * Actuals Collection
+         */
+        Actuals = uijet.Collection({
+            model   : Actual,
+            parse   : function (response) {
+                return response.actuals;
+            },
+            setUrl  : function (entity_url) {
+                this.url = entity_url;
             }
         });
 
--- openbudget/apps/projects/static/projects/explorer/ui/main.js
+++ openbudget/apps/projects/static/projects/explorer/ui/main.js
@@ -13,18 +13,25 @@ define([
         type    : 'Button',
         config  : {
             element : '#add_filter',
-            position: 'top:30px'
+            position: 'top:50px'
         }
     }, {
         type    : 'List',
         config  : {
-            element : '#filters_list'
+            element : '#filters_list',
+            position: 'fluid'
         }
     }, {
         type    : 'Pane',
         config  : {
-            element : '#entity_filter',
-            position: 'fluid'
+            element         : '#entity_filter',
+            dont_wake       : true,
+            mixins          : ['Transitioned'],
+            animation_type  : 'slide',
+            app_events      : {
+                'add_filter.clicked'            : 'wake',
+                'entity_filter_close.clicked'   : 'sleep'
+            }
         }
     }, {
         type    : 'Button',
@@ -35,16 +42,15 @@ define([
         type    : 'List',
         config  : {
             element     : '#entities_list',
-            dont_wake   : true,
             mixins      : ['Templated', 'Scrolled'],
             adapters    : ['jqWheelScroll', 'Spin'],
             resource    : 'Munis',
             signals     : {
                 pre_update      : 'spin',
-                post_fetch_data : 'spinOff'
-            },
-            app_events  : {
-                authenticated   : 'wake'
+                post_fetch_data : 'spinOff',
+                pre_wake        : function () {
+                    return ! this.has_content;
+                }
             }
         }
     }, {
@@ -53,24 +59,24 @@ define([
             element : '#nodes_picker',
             position: 'fluid'
         }
-    }, {
-        type    : 'List',
-        config  : {
-            element     : '#nodes_list',
-            dont_wake   : true,
-            mixins      : ['Templated', 'Scrolled'],
-            adapters    : ['jqWheelScroll', 'Spin'],
-            resource    : 'Nodes',
-            signals     : {
-                pre_update      : 'spin',
-                post_fetch_data : 'spinOff'
-            },
-            app_events  : {
-                'entities_list.selected'    : function ($selected) {
-                    var entity_id = $selected.attr('data-id');
-                }
-            }
-        }
+//    }, {
+//        type    : 'List',
+//        config  : {
+//            element     : '#nodes_list',
+//            dont_wake   : true,
+//            mixins      : ['Templated', 'Scrolled'],
+//            adapters    : ['jqWheelScroll', 'Spin'],
+//            resource    : 'Nodes',
+//            signals     : {
+//                pre_update      : 'spin',
+//                post_fetch_data : 'spinOff'
+//            },
+//            app_events  : {
+//                'entities_list.selected'    : function ($selected) {
+//                    var entity_id = $selected.attr('data-id');
+//                }
+//            }
+//        }
     }]);
 
     return Explorer;
