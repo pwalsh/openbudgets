3ee360a59e540915e2402c4e4b5d6b5c1521ec51
Yehonatan Daniv <maggotfish@gmail.com>
2013-06-04 11:18:01 +0300

    Implemented top section of nodes_picker pane; Refactored ui/main; Added some helper filters to Nodes collection

--- openbudget/apps/projects/static/projects/explorer/resources.js
+++ openbudget/apps/projects/static/projects/explorer/resources.js
@@ -20,7 +20,8 @@ define([
             parse   : function (response) {
                 //! Array.prototype.filter
                 return response.results.filter(function (item) {
-                    return item.division.index === 3 && (item.budgets.length || item.actuals.length);
+//                    return item.division.index === 3 && (item.budgets.length || item.actuals.length);
+                    return item.division.index === 3;
                 });
             }
         }),
@@ -34,8 +35,29 @@ define([
          * BudgetTemplateNodes Collection
          */
         Nodes = uijet.Collection({
-            model   : Node,
-            past    : function (node_id, past) {
+            model           : Node,
+            parse           : function (response) {
+                this._setAncestors(response);
+                return response;
+            },
+            roots           : function () {
+                return this.where({
+                    parent  : null
+                });
+            },
+            byAncestor      : function (ancestor_id) {
+                if ( ancestor_id ) {
+                    return this.filter(function (node) {
+                        return ~ node.attributes.ancestors.indexOf(ancestor_id);
+                    }).map(function (model) {
+                        return model.attributes;
+                    });
+                }
+                else {
+                    return this.toJSON();
+                }
+            },
+            past            : function (node_id, past) {
                 var node = this.get(node_id),
                     backwards = node.get('backwards');
                 past = past || [];
@@ -45,7 +67,7 @@ define([
                 }, this);
                 return past;
             },
-            future  : function (node_id, future) {
+            future          : function (node_id, future) {
                 var node = this.get(node_id),
                     forwards = node.get('forwards');
                 future = future || [];
@@ -55,8 +77,29 @@ define([
                 }, this);
                 return future;
             },
-            timeline: function (node_id) {
+            timeline        : function (node_id) {
                 return[node_id].concat(this.future(node_id), this.past(node_id));
+            },
+            _setAncestors   : function (response) {
+                var len = response.length,
+                    paths_lookup = {},
+                    node, n, route, path;
+                for ( n = len; node = response[n]; n-- ) {
+                    node.ancestors = [];
+                    paths_lookup[node.path] = node;
+                }
+                for ( n = len; node = response[n]; n-- ) {
+                    route = node.path.split('|').slice(1);
+                    node.level = route.length;
+                    while ( route.length ) {
+                        path = route.join('|');
+                        if ( path in paths_lookup ) {
+                            node.ancestors.push(paths_lookup[path].id);
+                        }
+                        route.shift();
+                    }
+                }
+                paths_lookup = null;
             }
         });
 
--- openbudget/apps/projects/static/projects/explorer/ui/main.js
+++ openbudget/apps/projects/static/projects/explorer/ui/main.js
@@ -4,6 +4,33 @@ define([
     'project_widgets/ClearableTextInput'
 ], function (uijet, Explorer) {
 
+    var listSearchHandler = function (value) {
+        if ( this.has_data && this.$children ) {
+            var results = this.search(value),
+                filter = function (i, item) {
+                    return ~ results.indexOf(+uijet.$(item).attr('data-id'));
+                };
+            this.$children.filter(filter).removeClass('removed');
+            this.$children.not(filter).addClass('removed');
+            this.publish('filtered');
+        }
+        else {
+            var _self = arguments.callee,
+                rendered_event = this.id + '.rendered';
+            if ( 'cached_value' in this ) {
+                this.cached_value = value;
+            }
+            else {
+                this.cached_value = value;
+                this.subscribe(rendered_event, function () {
+                    this.unsubscribe(rendered_event);
+                    _self.call(this, this.cached_value);
+                    delete this.cached_value;
+                });
+            }
+        }
+    };
+
     uijet.declare([{
         type    : 'Pane',
         config  : {
@@ -52,6 +79,11 @@ define([
                 keyup   : function (e) {
                     this.publish('changed', e.target.value);
                 }
+            },
+            signals     : {
+                post_wake   : function () {
+                    this.$element.focus();
+                }
             }
         }
     }, {
@@ -85,31 +117,7 @@ define([
                 }
             },
             app_events  : {
-                'entity_field.changed'  : function (value) {
-                    if ( this.has_data && this.$children ) {
-                        var results = this.search(value),
-                            filter = function (i, item) {
-                                return ~ results.indexOf(+uijet.$(item).attr('data-id'));
-                            };
-                        this.$children.filter(filter).removeClass('removed');
-                        this.$children.not(filter).addClass('removed');
-                        this.publish('filtered');
-                    }
-                    else {
-                        var _self = arguments.callee;
-                        if ( 'cached_value' in this ) {
-                            this.cached_value = value;
-                        }
-                        else {
-                            this.cached_value = value;
-                            this.subscribe('entities_list.rendered', function () {
-                                this.unsubscribe('entities_list.rendered');
-                                _self.call(this, this.cached_value);
-                                delete this.cached_value;
-                            });
-                        }
-                    }
-                },
+                'entity_field.changed'  : listSearchHandler,
                 'entities_list.filtered': 'scroll'
             }
         }
@@ -122,25 +130,61 @@ define([
     }, {
         type    : 'Pane',
         config  : {
-            element : '#nodes_filters',
+            element : '#nodes_filters_pane',
+            mixins  : ['Layered'],
             position: 'top:100 fluid'
+        }
+    }, {
+        type    : 'Button',
+        config  : {
+            element : '#filters_done'
+        }
+    }, {
+        type    : 'Button',
+        config  : {
+            element : '#filters_search'
+        }
+    }, {
+        type    : 'Pane',
+        config  : {
+            element     : '#nodes_search_pane',
+            mixins      : ['Layered'],
+            dont_wake   : true,
+            position    : 'top:100 fluid',
+            app_events  : {
+                'filters_search.clicked': 'wake'
+            }
         }
     }, {
         type    : 'ClearableTextInput',
         config  : {
-            element : '#nodes_search'
+            element : '#nodes_search',
+            button      : {
+                signals : {
+                    pre_click   : '-nodes_search.changed'
+                }
+            },
+            dom_events  : {
+                keyup   : function (e) {
+                    this.publish('changed', e.target.value);
+                }
+            },
+            signals     : {
+                post_wake   : function () {
+                    this.$element.focus();
+                }
+            }
         }
-//    }, {
-//        type    : 'List',
-//        config  : {
-//            element     : '#nodes_breadcrumbs',
+    }, {
+        type    : 'List',
+        config  : {
+            element     : '#nodes_breadcrumbs',
 //            adapters    : ['Breadcrumbs'],
-//            horizontal  : true,
-//            dont_wake   : true,
-//            app_events  : {
-//                'nodes_list.ready'  : function () {}
-//            }
-//        }
+            horizontal  : true,
+            app_events  : {
+                'nodes_list.ready'  : function () {}
+            }
+        }
     }, {
         type    : 'List',
         config  : {
@@ -163,34 +207,34 @@ define([
                 },
                 pre_update      : 'spin',
                 post_fetch_data : 'spinOff',
+                post_render     : function () {
+                    this.$children = this.$element.children();
+                    this.publish('rendered');
+                },
                 post_wake       : function () {
                     if ( this.changed ) {
-                        this.index().search_index.add(this.getData(this.context)
-                            //! Array.prototype.map
-                            .map(function (model) {
-                                return model.attributes;
-                            })
-                        );
+                        this.index()
+                            .search_index.add( this.resource.byAncestor() );
                         this.publish('ready', this.context);
                     }
                 }
             },
             app_events  : {
-                'entities_list.selected'    : function ($selected) {
+                'entities_list.selected': function ($selected) {
                     var entity_id = $selected.attr('data-id');
                     if ( this.latest_entity_id !== entity_id ) {
                         this.latest_entity_id = entity_id;
                         this.changed = true;
                         this.resource.url = API_URL + 'nodes/latest/' + entity_id + '/';
-                        this.wake({
-                            parent  : null
-                        });
+                        this.wake('roots');
                     }
                     else {
                         this.changed = false;
                         this.wake();
                     }
-                }
+                },
+                'nodes_search.changed'  : listSearchHandler,
+                'nodes_list.filtered'   : 'scroll'
             }
         }
     }]);
