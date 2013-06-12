ee6e0eab22fbf2373ee1355b6866b9923192b58f
Paul Walsh <paulywalsh@gmail.com>
2013-06-12 21:35:47 +0300

    Explicitly setting filters for entities

--- openbudget/apps/entities/filters.py
+++ openbudget/apps/entities/filters.py
@@ -10,8 +10,10 @@ class DomainFilter(FilterSet):
 class DivisionFilter(FilterSet):
     class Meta:
         model = Division
+        fields = ('budgeting', 'index')
 
 
 class EntityFilter(FilterSet):
     class Meta:
         model = Entity
+        fields = ('division__budgeting', 'parent')
