d1fe955de8b1381e7ae77443b177a14b7c8bbd65
Yehonatan Daniv <maggotfish@gmail.com>
2013-06-17 09:57:42 +0300

    PEP8 corrections

--- openbudget/apps/budgets/translation.py
+++ openbudget/apps/budgets/translation.py
@@ -1,14 +1,18 @@
 from modeltranslation.translator import translator, TranslationOptions
 from openbudget.apps.budgets import models
 
+
 class SheetTransOps(TranslationOptions):
     fields = ('description',)
+
 
 class SheetItemTransOps(TranslationOptions):
     fields = ('description',)
 
+
 class TemplateTransOps(TranslationOptions):
     fields = ('name', 'description')
+
 
 class TemplateNodeTransOps(TranslationOptions):
     fields = ('name', 'description')
