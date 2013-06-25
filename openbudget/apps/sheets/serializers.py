17aabe31754babf71892721652e3bd807182e4bb
Yehonatan Daniv <maggotfish@gmail.com>
2013-06-25 12:48:40 +0300

    Fixed url issues related to DenormalizedSheetItem; Replaced items with denormalizedsheetitems in fields of SheetDetail

--- openbudget/apps/sheets/serializers.py
+++ openbudget/apps/sheets/serializers.py
@@ -44,23 +44,28 @@ class TemplateDetail(TemplateBase):
         fields = TemplateBase.Meta.fields + ['nodes']
 
 
-class SheetBase(serializers.HyperlinkedModelSerializer):
+#TODO: change back to HyperlinkedModelSerializer once we fix the url of DenormalizedSheetItem
+class SheetBase(serializers.ModelSerializer):
     """The default serialized representation of sheets."""
 
     period = serializers.Field(source='period')
 
     class Meta:
         model = models.Sheet
-        fields = ['id', 'url', 'template', 'entity', 'description', 'period',
+        #TODO: put 'url' back here once we fix the url of DenormalizedSheetItem
+        fields = ['id', 'template', 'entity', 'description', 'period',
                   'created_on', 'last_modified'] + translated_fields(model)
 
-
-class SheetItemBase(serializers.HyperlinkedModelSerializer):
+#TODO: change back to HyperlinkedModelSerializer once we fix the url of DenormalizedSheetItem
+class SheetItemBase(serializers.ModelSerializer):
     """The default serialized representation of sheet items."""
+
+    url = serializers.HyperlinkedIdentityField(view_name='sheetitem-detail')
 
     class Meta:
         model = models.DenormalizedSheetItem
-        fields = ['id', 'url', 'budget', 'actual', 'description', 'code', 'name',
+        #TODO: put 'url' back here once we fix the url of DenormalizedSheetItem
+        fields = ['id', 'budget', 'actual', 'description', 'code', 'name',
                   'path', 'direction', 'parent', 'backwards'] + translated_fields(model)
 
 
@@ -73,7 +78,7 @@ class SheetDetail(SheetBase):
     items = SheetItemBase()
 
     class Meta(SheetBase.Meta):
-        fields = SheetBase.Meta.fields + ['items']
+        fields = SheetBase.Meta.fields + ['denormalizedsheetitems']
 
 
 class SheetItemDetail(SheetItemBase):
