9f7ca5bf61cc5c0e96a6ae3c5e9ae56702b7bf61
Yehonatan Daniv <maggotfish@gmail.com>
2013-06-30 12:30:08 +0300

    Fixed chart layout; Fixed updating of chart according to data;

--- openbudget/apps/projects/static/projects/explorer/widgets/TimelineChart.js
+++ openbudget/apps/projects/static/projects/explorer/widgets/TimelineChart.js
@@ -28,9 +28,9 @@ define([
                 height = element.offsetHeight,
 //                x = d3.time.scale()
                 x = d3.time.scale()
-                    .range([0, width]),
+                    .range([20, width - 20]),
                 y = d3.scale.linear()
-                    .range([height, 0]),
+                    .range([height - 20, 20]),
                 x_axis = d3.svg.axis()
                     .scale(x)
                     .orient('bottom')
@@ -53,9 +53,7 @@ define([
 
             this.svg = d3.select(this.$element[0]).append('svg')
                 .attr('width', width)
-                .attr('height', height)
-                .append('g');
-//                    .attr('transform', 'translate(20,20)');
+                .attr('height', height);
         },
         render          : function () {
             this._super();
@@ -66,12 +64,12 @@ define([
         },
         draw            : function (series) {
             var line = this.line,
+                colors = this.colors,
                 ids = [],
-                data = [],
-                colors = this.colors;
+                data = [];
 
             series.forEach(function (item) {
-                var id = item.cid,
+                var id = item.cid + '-' + item.get('updated'),
                     actual_id = id + '-actual',
                     budget_id = id + '-budget',
                     title = item.get('title'),
@@ -101,28 +99,36 @@ define([
                 d3.max(data, function (d) { return d3.max(d.values, amount); })
             ]);
 
+            // clean axes
+            this.svg.selectAll('.axis')
+                .remove();
+
             this.svg.append('g')
-                .attr('class', 'x_axis')
+                .attr('class', 'axis x_axis')
                 .attr('transform', 'translate(0,' + (this.height - 20) + ')')
                 .call(this.x_axis);
 
             this.svg.append('g')
-                .attr('class', 'y_axis')
-                .attr('transform', 'translate(20,0)')
+                .attr('class', 'axis y_axis')
+                .attr('transform', 'translate(10,0)')
                 .call(this.y_axis);
 
-            var timeline = this.svg.selectAll('.timeline')
-                .data(data)
-                .enter()
-                .append('g')
-                    .attr('class', 'timeline');
+            this.svg.selectAll('.timeline').remove();
 
-            timeline.append('path')
-                .attr('class', 'line')
-                .attr('d', function(d) {
-                    return line(d.values);
-                })
-                .style('stroke', function(d) { return colors(d.id); });
+            var timelines = this.svg.selectAll('.timeline')
+                .data(data, function (d) {
+                    return d.id;
+                });
+
+            timelines.enter()
+                .append('g')
+                    .attr('class', 'timeline')
+                    .append('path')
+                        .attr('class', 'line')
+                        .attr('d', function(d) {
+                            return line(d.values);
+                        })
+                        .style('stroke', function(d) { return colors(d.id); });
         }
     });
 
