a0319e063359a386f688b2c0f33493274f55975b
Paul Walsh <paulywalsh@gmail.com>
2013-07-01 12:01:46 +0300

    Updating the v1 endpoint list

--- openbudget/api/v1/views.py
+++ openbudget/api/v1/views.py
@@ -8,14 +8,15 @@ def api_v1(request):
     """The entry endpoint of our v1 API"""
 
     return Response({
-        'domains': reverse('domain-list', request=request),
-        'divisions': reverse('division-list', request=request),
-        'entities': reverse('entity-list', request=request),
-        'templates': reverse('template-list', request=request),
-        'templates/nodes/': reverse('templatenode-list', request=request),
-        'sheets': reverse('sheet-list', request=request),
-        'sheets/items/': reverse('sheetitem-list', request=request),
-        'contexts': reverse('context-list', request=request),
-        'projects': reverse('project-list', request=request),
-        'projects/states/': reverse('state-list', request=request),
+        'Domains': reverse('domain-list', request=request),
+        'Divisions': reverse('division-list', request=request),
+        'Entities': reverse('entity-list', request=request),
+        'Templates': reverse('template-list', request=request),
+        'Template Nodes': reverse('templatenode-list', request=request),
+        'Sheets': reverse('sheet-list', request=request),
+        'Sheet Items': reverse('sheetitem-list', request=request),
+        #'Sheet Item Timeline': reverse('sheetitem-timeline', request=request),
+        'Contexts': reverse('context-list', request=request),
+        'Projects': reverse('project-list', request=request),
+        'Project States': reverse('state-list', request=request),
     })
