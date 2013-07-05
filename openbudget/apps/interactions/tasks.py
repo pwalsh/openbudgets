adc782e6d6d5e2ee02da3de9c4e52dd772af267d
Paul Walsh <paulywalsh@gmail.com>
2013-07-05 08:48:28 +0300

    unregistering task not being used

--- openbudget/apps/interactions/tasks.py
+++ openbudget/apps/interactions/tasks.py
@@ -3,18 +3,18 @@ from django.core.mail import send_mail
 from celery.task import task
 
 
-@task(name="tasks.send_notification")
-def send_notification():
-
-    subject = ''
-    message = ''
-    sender = ''
-    recipient = ''
-
-    send_response = send_mail(
-        subject,
-        message,
-        sender,
-        [recipient],
-        fail_silently=True
-    )
+#@task(name="tasks.send_notification")
+#def send_notification():
+#
+#    subject = ''
+#    message = ''
+#    sender = ''
+#    recipient = ''
+#
+#    send_response = send_mail(
+#        subject,
+#        message,
+#        sender,
+#        [recipient],
+#        fail_silently=True
+#    )
