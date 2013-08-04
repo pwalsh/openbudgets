61439cceafda70b5ef32178c8495ba73bfa4f1e8
Yehonatan Daniv <maggotfish@gmail.com>
2013-08-04 15:45:30 +0300

    Fixed path of search module

--- openbudget/apps/projects/static/projects/comparisons/comparisons.js
+++ openbudget/apps/projects/static/projects/comparisons/comparisons.js
@@ -10,7 +10,7 @@ define([
     'modules/engine/mustache',
     'modules/xhr/jquery',
     'modules/animation/uijet-transit',
-    'modules/uijet-search',
+    'modules/search/uijet-search',
     'project_modules/uijet-i18n'
 ], function (uijet, resources, api, Backbone, Router, $, Ebox, Q, Mustache) {
 
