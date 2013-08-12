0dcd50c19d0029ec01bf00ee44378208427f866a
Yehonatan Daniv <maggotfish@gmail.com>
2013-08-12 11:00:49 +0300

    Fixed text for previous search terms prefix

--- openbudget/apps/entities/static/entities/explorer/dictionary.js
+++ openbudget/apps/entities/static/entities/explorer/dictionary.js
@@ -1,7 +1,7 @@
 define(function() {
     return {
         'search-menu-free'  : gettext('Free text search'),
-        'search-term-prefix': gettext('Search for')
+        'search-term-prefix': gettext('Search for "')
     };
 
 });
