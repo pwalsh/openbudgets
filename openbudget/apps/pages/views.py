30e2285a73e496f9c71fa50adf5a9dc381b3e59d
Paul Walsh <paulywalsh@gmail.com>
2013-08-14 09:26:58 +0300

    UI Fixes

--- openbudget/apps/pages/views.py
+++ openbudget/apps/pages/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView, DetailView, FormView
+from django.http import HttpResponse
 from openbudget.apps.pages.models import Page
 from openbudget.apps.pages.forms import ContactForm
 
@@ -15,6 +16,51 @@ class HomeView(TemplateView):
 class ContactView(FormView):
     form_class = ContactForm
     template_name = 'pages/contact.html'
+
+    def form_invalid(self, form, request=None):
+
+        response = super(ContactView, self).form_invalid(form)
+
+        if self.request.is_ajax():
+
+            # we are simplifying the errors because of the available UI space.
+
+            if 'email' in dict(form.errors):
+
+                chosen_error = form.errors['email'][0]
+
+            elif 'name' in dict(form.errors):
+
+                chosen_error = form.errors['name'][0]
+
+            else:
+
+                chosen_error = form.errors['__all__'][0]
+
+            data = chosen_error
+
+            # JQuery is expecting a string, not an object.
+            #return self.render_to_json_response(data, status=400)
+            return HttpResponse(data)
+
+        else:
+
+            return response
+
+    def form_valid(self, request, form):
+
+        response = super(ContactView, self).form_valid(request, form)
+
+        if self.request.is_ajax():
+            data = {
+                'data': _('Check your email to proceed'),
+            }
+
+            return self.render_to_json_response(data)
+
+        else:
+
+            return response
 
 
 class PageView(DetailView):
