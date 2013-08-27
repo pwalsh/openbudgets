1e0cf0f9b4aa6bb7d6824b446db97ab05e7ff53e
Paul Walsh <paulywalsh@gmail.com>
2013-08-27 13:10:58 +0300

    Reinstating the API browser

--- openbudget/settings/base.py
+++ openbudget/settings/base.py
@@ -231,7 +231,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.UnicodeJSONRenderer',
-        #'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework.filters.SearchFilter',
