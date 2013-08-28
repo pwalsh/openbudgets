1ef84476312717aa6373157ba13a1733e9042137
Yehonatan Daniv <maggotfish@gmail.com>
2013-08-28 17:00:52 +0300

    Updated ProjectBaseSerializer to use latest state of Project model; Changed ProjectBaseSerializer to inherit ModelSerializer so it will work also in UI views; Changed ProjectBaseSerializer.author to use AccountMin serializer

--- openbudget/apps/projects/serializers.py
+++ openbudget/apps/projects/serializers.py
@@ -4,16 +4,16 @@ from openbudget.apps.projects import models
 from openbudget.apps.accounts.serializers import AccountMin
 
 
-class ProjectBaseSerializer(HyperlinkedModelSerializer):
+class ProjectBaseSerializer(ModelSerializer):
     """Base Project serializer, exposing our defaults for projects."""
 
-    owner = RelatedField()
-    author = RelatedField()
+    author = AccountMin()
+    url = Field(source='get_absolute_url')
 
     class Meta:
         model = models.Project
-        fields = ['url', 'id', 'owner', 'author', 'name', 'description', 'featured',
-                  'preview', 'created_on', 'last_modified'] +\
+        fields = ['url', 'uuid', 'author', 'label', 'description', 'featured',
+                  'screenshot', 'created_on', 'last_modified'] +\
                  translated_fields(model)
         lookup_field = 'uuid'
 
