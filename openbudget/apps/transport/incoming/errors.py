5479e7aafc864964c2b1f79b09c95244e6c3a1c5
Paul Walsh <paulywalsh@gmail.com>
2013-09-01 12:04:51 +0300

    All validation error messages need to use unicode strings.

--- openbudget/apps/transport/incoming/errors.py
+++ openbudget/apps/transport/incoming/errors.py
@@ -1,55 +1,70 @@
+import json
 from django.core.exceptions import NON_FIELD_ERRORS
 from django.utils.translation import ugettext_lazy as _
 
 
-class DataInputError(object):
+UNKNOWN = u'Unknown'
+
+
+class ErrorMessage(object):
+
+    def get_arguments(self):
+        # a dictionary for formatting the _message.
+        raise NotImplementedError
+
+    def to_json(self):
+        value = json.dumps(self.get_arguments(), ensure_ascii=False)
+        return value
+
+
+class DataInputError(ErrorMessage):
 
     def __init__(self, row=None, columns=None, values=None):
         if row is None:
-            row = 'Unknown'
+            row = UNKNOWN
         self.row = row
-        self.columns = columns or ['Unknown']
-        self.values = values or ['Unknown']
+        self.columns = columns or [UNKNOWN]
+        self.values = values or [UNKNOWN]
 
-    def to_json(self):
+    def get_arguments(self):
         return {
-            'row': self.row,
-            'columns': self.columns,
-            'values': self.values,
-            'message': self.message
+            u'row': self.row,
+            u'columns': self.columns,
+            u'values': self.values,
+            u'message': self.message
         }
 
     @property
     def _message(self):
-        return _('Error found in row: {row}; and columns: {columns}; '
-                 'with values: {values}')
+        return _(u'Error found in row: {row}; and columns: {columns}; '
+                 u'with values: {values}')
 
     @property
     def message(self):
         return self._message.format(row=self.row,
-                                    columns=', '.join(self.columns),
-                                    values=', '.join(self.values))
+                                    columns=u', '.join(self.columns),
+                                    values=u', '.join(self.values))
 
 
-class DataCollisionError(object):
+class DataCollisionError(ErrorMessage):
 
     def __init__(self, rows=None, columns=None, values=None):
-        self.rows = rows or ['Unknown', 'Unknown']
-        self.columns = columns or ['Unknown']
-        self.values = values or ['Unknown']
+        self.rows = rows or [UNKNOWN, UNKNOWN]
+        self.columns = columns or [UNKNOWN]
+        self.values = values or [UNKNOWN]
 
-    def to_json(self):
+    def get_arguments(self):
         return {
-            'rows': self.rows,
-            'columns': self.columns,
-            'values': self.values,
-            'message': self.message
+            u'rows': self.rows,
+            u'columns': self.columns,
+            u'values': self.values,
+            u'message': self.message
         }
 
     @property
     def _message(self):
-        return _('Source data collision error in rows: {first_row}, '
-                 '{second_row}; and columns: {columns}; with values: {values}')
+        return _(u'Source data collision error in rows: {first_row}, '
+                 u'{second_row}; and columns: {columns}; with values: {values}')
 
     @property
     def message(self):
@@ -61,98 +76,98 @@ class DataCollisionError(object):
 class DataSyntaxError(DataInputError):
 
     def __unicode__(self):
-        return _('Data Syntax Error')
+        return _(u'Data Syntax Error')
 
     @property
     def _message(self):
-        return _('Syntax error found in row: {row}; and columns: {columns}; '
-                 'with values: {values}')
+        return _(u'Syntax error found in row: {row}; and columns: {columns}; '
+                 u'with values: {values}')
 
 
 class DataAmbiguityError(DataCollisionError):
 
     def __unicode__(self):
-        return _('Data Ambiguity Error')
+        return _(u'Data Ambiguity Error')
 
     @property
     def _message(self):
-        return _('Source contains siblings with same code in rows: {first_row},'
-                 ' {second_row}; and columns: {columns}; with values: {values}')
+        return _(u'Source contains siblings with same code in rows: {first_row},'
+                 u' {second_row}; and columns: {columns}; with values: {values}')
 
 
-class MetaParsingError(object):
+class MetaParsingError(ErrorMessage):
 
-    def __init__(self, reason='Unknown'):
+    def __init__(self, reason=UNKNOWN):
         self.reason = reason
 
-    def to_json(self):
+    def get_arguments(self):
         return {
-            'reason': self.reason,
-            'message': self.message
+            u'reason': self.reason,
+            u'message': self.message
         }
 
     def __unicode__(self):
-        return _('Meta Parsing Error')
+        return _(u'Meta Parsing Error')
 
     @property
     def message(self):
-        return _('Source meta data invalid for reason: {reason}').format(
+        return _(u'Source meta data invalid for reason: {reason}').format(
             reason=self.reason)
 
 
 class DataValidationError(DataInputError):
 
-    def __init__(self, reasons=None, row='Unknown'):
+    def __init__(self, reasons=None, row=UNKNOWN):
         super(DataValidationError, self).__init__(row=row)
         self.reasons = reasons
 
-    def to_json(self):
-        error_dic = super(DataValidationError, self).to_json()
-        error_dic['reasons'] = self.reasons
+    def get_arguments(self):
+        error_dic = super(DataValidationError, self).get_arguments()
+        error_dic[u'reasons'] = self.reasons
         return error_dic
 
     def __unicode__(self):
-        return _('Data Validation Error')
+        return _(u'Data Validation Error')
 
     @property
     def message(self):
-        reasons = 'Unknown'
+        reasons = UNKNOWN
 
         if self.reasons:
             reasons = []
             for key, messages in self.reasons.iteritems():
                 if key == NON_FIELD_ERRORS:
-                    key = 'others'
+                    key = u'others'
 
-                reasons.append('{key}: {messages}'.format(
+                reasons.append(u'{key}: {messages}'.format(
                     key=key,
-                    messages='and '.join(messages)))
+                    messages=u'and '.join(messages)))
 
-            reasons = ' AND '.join(reasons)
+            reasons = u' AND '.join(reasons)
 
-        return _('Source data invalid in row: {row}; '
-                 'for reasons: {reasons}').format(row=self.row, reasons=reasons)
+        return _(u'Source data invalid in row: {row}; '
+                 u'for reasons: {reasons}').format(row=self.row, reasons=reasons)
 
 
 class NodeDirectionError(DataCollisionError):
 
     def __unicode__(self):
-        return _('Node Direction Error')
+        return _(u'Node Direction Error')
 
     @property
     def _message(self):
-        return _("Inverse node's direction is not opposite of item in row: "
-                 "{row}; and inverse in row: {row}")
+        return _(u"Inverse node's direction is not opposite of item in row: "
+                 u"{row}; and inverse in row: {row}")
 
 
 class ParentScopeError(DataInputError):
 
     def __unicode__(self):
-        return _('Parent Scope Error')
+        return _(u'Parent Scope Error')
 
     @property
     def _message(self):
-        return _("Parent scope is missing or not resolvable in row: {row}")
+        return _(u'Parent scope is missing or not resolvable in row: {row}')
 
     @property
     def message(self):
@@ -163,42 +178,42 @@ class NodeNotFoundError(DataInputError):
 
     @property
     def _message(self):
-        return _('Budget template node not found for item in'
-                 ' row: {row}; and columns: {columns}; with values: {values}')
+        return _(u'Budget template node not found for item in '
+                 u'row: {row}; and columns: {columns}; with values: {values}')
 
 
 class ParentNodeNotFoundError(DataInputError):
 
     @property
     def _message(self):
-        return _('Parent node not found for item in '
-                 'row: {row}; and columns: {columns}; with values: {values}')
+        return _(u'Parent node not found for item in '
+                 u'row: {row}; and columns: {columns}; with values: {values}')
 
 
 class PathInterpolationError(DataInputError):
 
     def __unicode__(self):
-        return _('Nodes Path Interpolation Error')
+        return _(u'Nodes Path Interpolation Error')
 
     @property
     def _message(self):
-        return _('Interpolation failed, no ancestor found for row: {row}')
+        return _(u'Interpolation failed, no ancestor found for row: {row}')
 
 
 class InverseScopesError(DataInputError):
 
     def __unicode__(self):
-        return _('Inverse Scopes Error')
+        return _(u'Inverse Scopes Error')
 
     @property
     def _message(self):
-        return _('Inverse scopes not matching inverses in'
-                 ' row: {row}; and columns: {columns}; with values: {values}')
+        return _(u'Inverse scopes not matching inverses in '
+                 u'row: {row}; and columns: {columns}; with values: {values}')
 
 
 class InverseNodeNotFoundError(DataInputError):
 
     @property
     def _message(self):
-        return _('Inverse node not found for item in'
-                 ' row: {row}; and columns: {columns}; with values: {values}')
+        return _(u'Inverse node not found for item in '
+                 u'row: {row}; and columns: {columns}; with values: {values}')
