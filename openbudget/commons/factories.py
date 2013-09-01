264c7eea4cf3bf5073bbc2e042a83ae1fa60606a
Paul Walsh <paulywalsh@gmail.com>
2013-09-01 12:07:32 +0300

    Code style

--- openbudget/commons/factories.py
+++ openbudget/commons/factories.py
@@ -7,8 +7,7 @@ class SiteFactory(factory.Factory):
     FACTORY_FOR = Site
 
     domain = factory.Sequence(
-        lambda n: 'domain{0}.com'.format(n)
-    )
+        lambda n: 'domain{0}.com'.format(n),)
+
     name = factory.Sequence(
-        lambda n: 'Domain{0} Name'.format(n)
-    )
+        lambda n: 'Domain{0} Name'.format(n),)
