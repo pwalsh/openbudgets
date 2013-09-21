46d7ce8cbc9a6431e36601793c21265adc7681ef
Paul Walsh <paulywalsh@gmail.com>
2013-09-21 16:00:09 +0300

    Naive helper function for setting comparable value

--- openbudget/apps/sheets/utilities.py
+++ openbudget/apps/sheets/utilities.py
@@ -1,13 +1,9 @@
 from django.conf import settings
 
 
-def is_comparable(instance):
+def is_comparable():
 
     """Sets the value of TemplateNode.comparable to True or False."""
 
-    value = True
-    if instance.is_blueprint:
-        value = settings.OPENBUDGETS_TEMPLATENODE_COMPARABLE_DEFAULT
-    elif not instance.is_blueprint:
-        value = settings.OPENBUDGETS_TEMPLATENODE_NOT_IN_BLUEPRINT_COMPARABLE_DEFAULT
+    value = settings.OPENBUDGETS_TEMPLATENODE_COMPARABLE_DEFAULT
     return value
