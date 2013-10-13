374312d43e8c07773112ffc5ee0b4f3bd66dfd13
Paul Walsh <paulywalsh@gmail.com>
2013-10-13 09:44:07 +0300

    Adding View that got lost

--- openbudgets/apps/tools/views/ui.py
+++ openbudgets/apps/tools/views/ui.py
@@ -1,12 +1,12 @@
 from django.views.generic import DetailView, ListView
 from rest_framework.renderers import JSONRenderer
 from openbudgets.apps.accounts.serializers import AccountMin
-from openbudgets.apps.tools.models import Tool
+from openbudgets.apps.tools import models
 from openbudgets.apps.tools import serializers
 
 
 class ToolListView(ListView):
-    model = Tool
+    model = models.Tool
     template_name = 'tools/tool_list.html'
 
     def get_context_data(self, **kwargs):
@@ -28,11 +28,24 @@ class ToolListView(ListView):
         return context
 
 
-class ToolDetailView(DetailView):
-    model = Tool
+class ToolDetailBaseView(DetailView):
+
+    model = models.Tool
+
+    def get_context_data(self, **kwargs):
+
+        context = super(ToolDetailBaseView, self).get_context_data(**kwargs)
+        renderer = JSONRenderer()
+
+        context['tool_json'] = renderer.render(serializers.Tool(self.object, context={'request': self.request}).data)
+
+        return context
+
+
+class ToolDetailView(ToolDetailBaseView):
 
     def get_template_names(self):
-        return ['tools/ext/{slug}.html'.format(slug=self.object.slug)]
+        return ['tools/ext/{slug}/tool.html'.format(slug=self.object.slug)]
 
     def get_context_data(self, **kwargs):
 
@@ -46,6 +59,28 @@ class ToolDetailView(DetailView):
             user_object = AccountMin(user).data
 
         context['user_json'] = renderer.render(user_object)
-        context['tool_json'] = renderer.render(serializers.Tool(self.object).data)
 
         return context
+
+
+class ToolEmbedView(ToolDetailBaseView):
+
+    def get_template_names(self):
+        return ['projects/ext/{slug}/embed.html'.format(slug=self.object.slug)]
+
+    def get_context_data(self, **kwargs):
+
+        context = super(ToolEmbedView, self).get_context_data(**kwargs)
+        state = {}
+        renderer = JSONRenderer()
+
+        try:
+            state_id = self.kwargs.get('state')
+            state = serializers.State(models.State.objects.get(id=state_id), context={'request': self.request}).data
+        except models.State.DoesNotExist:
+            pass
+
+        context['state_json'] = renderer.render(state)
+
+        return context
+
