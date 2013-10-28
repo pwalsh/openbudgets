3b986dd2dae871497ffed58af067542f79940c0a
Paul Walsh <paulywalsh@gmail.com>
2013-10-28 22:07:10 +0200

    Remving the browsable API, again, for performance reasons

--- openbudgets/settings/__init__.py
+++ openbudgets/settings/__init__.py
@@ -229,7 +229,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.UnicodeJSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        #'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework.filters.SearchFilter',
