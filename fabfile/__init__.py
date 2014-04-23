ba9cf5e3615f3f573ab38d86fb660b9c552bd6e6
Paul Walsh <paulywalsh@gmail.com>
2014-04-23 16:59:35 +0300

    Adding a mock function to populate the database with dummy data.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -2,3 +2,10 @@ from fabric.contrib import django
 django.project('openbudgets')
 from quilt import *
 from dock.fabfile import *
+
+
+@task
+def mock(amount=300):
+    utilities.notify(u'Creating some mock objects for the database.')
+    from openbudgets.commons.data import mock_db
+    mock_db(amount)
