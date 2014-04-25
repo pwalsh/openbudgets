18925af2a74c20e86867bce9c480b5cd710b6b32
Paul Walsh <paulywalsh@gmail.com>
2014-04-25 11:05:58 +0300

    Set comparable state of node.

--- openbudgets/apps/sheets/utilities.py
+++ openbudgets/apps/sheets/utilities.py
@@ -1,9 +1,20 @@
 from django.conf import settings
 
 
-def is_comparable():
+def is_node_comparable(instance):
 
-    """Sets the value of TemplateNode.comparable to True or False."""
+    """Sets the value of TemplateNode.comparable to True or False.
 
-    value = settings.OPENBUDGETS_COMPARABLE_TEMPLATENODE_DEFAULT
+    Relies on the non-abstract TemplateNode implementation where nodes
+    can belong to many templates.
+
+    """
+
+    value = settings.OPENBUDGETS_COMPARABLE_TEMPLATENODE
+
+    if all([t.is_blueprint for t in instance.templates.all()]):
+        value = settings.OPENBUDGETS_COMPARABLE_TEMPLATENODE_IN_BLUEPRINT
+    else:
+        value = settings.OPENBUDGETS_COMPARABLE_TEMPLATENODE_NOT_IN_BLUEPRINT
+
     return value
