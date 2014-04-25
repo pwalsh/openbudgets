5060fcdbb54da8120d893c72bb959969c9b1d9d4
Paul Walsh <paulywalsh@gmail.com>
2014-04-25 11:12:25 +0300

    Refactor our custom field serializers

--- openbudgets/commons/serializers.py
+++ openbudgets/commons/serializers.py
@@ -1,23 +1,32 @@
-from rest_framework.relations import SlugRelatedField, PrimaryKeyRelatedField
+from rest_framework import relations
 
 
-class UUIDRelatedField(SlugRelatedField):
-    """
-    Represents a relationship using a UUID field on the target.
+class UUIDRelatedField(relations.SlugRelatedField):
+
+    """Returns a string representation of a UUID instance, where the relation is on a `uuid` field.
+
+    This is required do to the way the UUIDField we depend on implements
+    its to_python method.
+
     """
 
     def __init__(self, *args, **kwargs):
-        self.slug_field = 'uuid'
-        super(SlugRelatedField, self).__init__(*args, **kwargs)
+        super(UUIDRelatedField, self).__init__(*args, slug_field='uuid', **kwargs)
 
-    def to_native(self, value):
-        return str(value.uuid)
+    def to_native(self, obj):
+        return str(getattr(obj, self.slug_field))
 
 
-class UUIDPKRelatedField(PrimaryKeyRelatedField):
-    """
-    Represents a relationship using a UUID field on the target.
+class UUIDPrimaryKeyRelatedField(relations.PrimaryKeyRelatedField):
+
+    """Returns a string representation of a UUID instance, where the `pk` is a UUID.
+
+    This is required do to the way the UUIDField we depend on implements
+    its to_python method.
+
     """
 
     def to_native(self, pk):
+        if not pk:
+            return None
         return str(pk)
