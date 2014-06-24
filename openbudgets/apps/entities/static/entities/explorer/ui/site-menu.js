0246c735fe08916a8a46c23dd86f10905306f61e
Yehonatan Daniv <maggotfish@gmail.com>
2014-06-24 10:39:44 +0300

    Fixed issue with header's layer dropping in z index

--- openbudgets/apps/entities/static/entities/explorer/ui/site-menu.js
+++ openbudgets/apps/entities/static/entities/explorer/ui/site-menu.js
@@ -15,12 +15,12 @@ define([
             click_event : 'mouseover',
             signals     : {
                 post_init   : function () {
-                    this.header_anim_props = { translateX: - this.$element[0].offsetWidth };
+                    // use right since transforms cause the header's layer to lose its proper z-index
+                    this.header_anim_props = { right: this.$element[0].offsetWidth };
                     this.anim_props = { translateX: -276 };
 
                     this.$header = uijet.$('#sheet_header_top_container');
                     this.header_anim_callback = function () {
-                        this.$header.removeClass('transitioned');
                         uijet.publish('sheet_header_moved');
                     }.bind(this);
 
@@ -33,7 +33,7 @@ define([
                     uijet.animate(this.$element, this.anim_props, this.anim_options);
                 },
                 close_comments  : function () {
-                    uijet.animate(this.$header, { translateX: 0 }, this.anim_options, this.header_anim_callback);
+                    uijet.animate(this.$header, { right : 0 }, this.anim_options, this.header_anim_callback);
                     uijet.animate(this.$element, { translateX: 0 }, this.anim_options);
                 }
             }
