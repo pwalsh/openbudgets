f4287c04f572819dcbbd47ad34e498a9f4039741
Paul Walsh <paulywalsh@gmail.com>
2014-06-30 22:37:11 +0300

    Add Redactor RTE for Page content in admin.

--- openbudgets/apps/pages/admin.py
+++ openbudgets/apps/pages/admin.py
@@ -1,10 +1,14 @@
+from django.db import models
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
+from redactor.widgets import AdminRedactorEditor
 from openbudgets.apps.pages.models import Page
 
 
 class PageAdmin(TranslationAdmin):
-    pass
+    formfield_overrides = {
+        models.TextField: {'widget': AdminRedactorEditor},
+    }
 
 
 admin.site.register(Page, PageAdmin)
