f706db2b9b068e1ceacdfb7e8d63a9f07dfb8742
Yehonatan Daniv <maggotfish@gmail.com>
2014-07-01 19:52:38 +0300

    Implemented contextual data widget

--- openbudgets/apps/entities/static/entities/explorer/ui/main.js
+++ openbudgets/apps/entities/static/entities/explorer/ui/main.js
@@ -4,6 +4,7 @@ define([
     'ui/site-menu',
     'ui/sheet-selector',
     'ui/sheet',
+    'ui/contextual-data',
     'ui/breadcrumbs',
     'ui/items-list',
     'ui/bars',
@@ -17,11 +18,12 @@ define([
     'uijet_dir/mixins/Scrolled',
     'uijet_dir/adapters/Spin',
     'uijet_dir/adapters/jqWheelScroll'
-], function (uijet, explorer, site_menu, sheet_selector, sheet, breadcrumbs, items_list, bars, comments, footer) {
+], function (uijet, explorer, site_menu, sheet_selector, sheet, contextual_data, breadcrumbs, items_list, bars, comments, footer) {
 
     uijet.declare(site_menu)
         .declare(sheet_selector)
         .declare(sheet)
+        .declare(contextual_data)
         .declare(breadcrumbs)
         .declare(items_list)
         .declare(bars)
